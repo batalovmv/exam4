b50bc63c45839bfe7e3d504f1d1775170eb91da5
Баталов Михаил <batalov94@gmail.com>
2023-03-11 17:34:33 +0600

    added css

--- task1/js/index.js
+++ task1/js/index.js
@@ -1,45 +1,43 @@
 
-let secondColumn 
+let secondColumn
 let thirdColumn
-document.addEventListener("DOMContentLoaded", function (event) {
-
-
-
-
-  
-    let keys = Object.keys(localStorage);
-    for (let key of keys) {
-       secondColumn = document.getElementById('inProgress').innerHTML
-       thirdColumn = document.getElementsByClassName('done-title')[0].innerHTML
-      if (localStorage.getItem(key) === secondColumn) {
-console.log('случай 1 '+key);
-        document.querySelector(".todo-inner").appendChild(document.querySelector(".task-box").cloneNode(true));
-
-        document.querySelector(".box-text").innerHTML = key
-        document.querySelector("#next").innerHTML = secondColumn
-        document.querySelector(".task-box").style.display = "block";
-
-      } else if (localStorage.getItem(key) === thirdColumn) {
-        console.log('случай 2 '+key);
-        document.querySelector(".progress-inner").appendChild(document.querySelector(".task-box").cloneNode(true));
-        document.querySelector(".box-text").innerHTML = key
-        document.querySelector("#next").innerHTML = thirdColumn
-        document.querySelector(".task-box").style.display = "block";
-      } else if (localStorage.getItem(key) === deleteTitle) {
-        console.log('случай 3');
-        document.querySelector(".done-inner").appendChild(document.querySelector(".task-box").cloneNode(true));
-        document.querySelector(".box-text").innerHTML = key
-        document.querySelector("#next").innerHTML = deleteTitle
-        document.querySelector(".task-box").style.display = "block";
-      }
-
+document.addEventListener("DOMContentLoaded", function () {
+  let keys = Object.keys(localStorage);
+  for (let key of keys) {
+    secondColumn = document.getElementById('inProgress').innerHTML
+    thirdColumn = document.getElementsByClassName('done-title')[0].innerHTML
+    if (localStorage.getItem(key) === secondColumn) {
+      document.querySelector(".todo-inner").append(document.querySelector(".task-box").cloneNode(true));
+      document.querySelector(".box-text").innerHTML = key
+      document.querySelector("#next").innerHTML = secondColumn
+      document.querySelector(".task-box").style.display = "block";
+      alert('случай 1')
+    } else if (localStorage.getItem(key) === thirdColumn) {
+      document.querySelector(".progress-inner").append(document.querySelector(".task-box").cloneNode(true));
+      document.querySelector(".box-text").innerHTML = key
+      document.querySelector("#next").innerHTML = thirdColumn
+      document.querySelector(".task-box").style.display = "block";
+      alert('случай 2')
+    } else if (localStorage.getItem(key) === deleteTitle) {
+      alert('случай 3');
+      document.querySelector(".done-inner").append(document.querySelector(".task-box").cloneNode(true));
+      document.querySelector(".box-text").innerHTML = key
+      document.querySelector("#next").innerHTML = deleteTitle
+      document.querySelector(".task-box").style.display = "block";
+    } else {
+      alert('случай 4');
+      document.querySelector(".done-inner").append(document.querySelector(".task-box").cloneNode(true));
+      document.querySelector(".box-text").innerHTML = key
+      document.querySelector("#next").innerHTML = deleteTitle
+      document.querySelector(".task-box").style.display = "block";
     }
-  
-  console.log(document.getElementById('progress-title'));
-  console.log(document.querySelector([id = "progress-title"]));
+
+  }
 
 
-  
+
+
+
 });
 let deleteTitle = 'Delete'
 function btnInput() {
@@ -61,31 +59,29 @@ function btnDelete() {
   });
 }
 
+
 function btnNext() {
   let div = document.querySelectorAll('.task-box');
-  let i = 0
   div.forEach((e) => {
+
+
     e.onclick = function () {
       let delKey = this.querySelector('.box-text').innerHTML
-      console.log(delKey);
-      console.log(localStorage.getItem(delKey));
-
-      if (localStorage.getItem(delKey) === document.getElementsByClassName('progress-title')[0].textContent) {
+      if (this.querySelector('#next').innerHTML === document.getElementsByClassName('progress-title')[0].textContent) {
         console.log('бинго');
         let nextColumn = document.getElementsByClassName('done-title')[0].textContent
+        alert(document.getElementsByClassName('done-title')[0].textContent)
+        alert(this.querySelector('#next').innerHTML)
         console.log(nextColumn);
-        localStorage.setItem(delKey, nextColumn)
 
-      } else if (localStorage.getItem(delKey) === document.getElementsByClassName('done-title')[i].textContent) {
-        console.log('бинго2');
+        localStorage.setItem(delKey, nextColumn)
+      } else if (localStorage.getItem(delKey) === document.getElementsByClassName('done-title')[0].textContent) {
 
         localStorage.setItem(delKey, deleteTitle)
       } else {
         alert('ПОздравляю вы завершили задание')
         let delKey = this.querySelector('.box-text').innerHTML
         localStorage.removeItem(delKey)
-        this.remove()
-
 
       }
 
